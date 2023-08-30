27a47f16c60474868939255eaea47a792fb2145c
Kacper Kafara <kacperkafara@gmail.com>
2023-08-30 21:48:13 +0200

    Initial commit

---
+++ app/src/main/java/com/foo/bar/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.foo.bar
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.foo.bar.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var openModalButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        openModalButton = mainBinding.openButton
+
+        openModalButton.setOnClickListener {
+//            this.createAndShowModal()
+            attachSomeContent()
+        }
+
+
+        setContentView(mainBinding.root)
+    }
+
+    private fun createAndShowModal() {
+        val modal = Modal()
+        modal.show(supportFragmentManager, Modal.TAG)
+    }
+
+    private fun attachSomeContent() {
+//       mainBinding.root.addView()
+        mainBinding.root.addView(createModalViewContent(), ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        ))
+        mainBinding.root.requestLayout()
+    }
+
+    private fun createModalViewContent(): View {
+        val coordinatorLayout = CoordinatorLayout(this)
+        val frameLayout = FrameLayout(this)
+        val view = View(this)
+
+        view.setBackgroundColor(Color.GREEN)
+        frameLayout.setBackgroundColor(Color.BLUE)
+//        coordinatorLayout.setBackgroundColor(Color.YELLOW)
+
+        frameLayout.addView(view, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT))
+
+        coordinatorLayout.addView(frameLayout, CoordinatorLayout.LayoutParams(
+            CoordinatorLayout.LayoutParams.MATCH_PARENT,
+            CoordinatorLayout.LayoutParams.MATCH_PARENT
+        ))
+
+        val frameLayoutParams = frameLayout.layoutParams as CoordinatorLayout.LayoutParams
+        frameLayoutParams.behavior = BottomSheetBehavior<FrameLayout>()
+
+        val behavior = BottomSheetBehavior.from(frameLayout)
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        coordinatorLayout.requestLayout()
+
+
+        return coordinatorLayout
+    }
+
+
+}
