eaf6474e49de771c7e4e2cae4b337edfc8bad6a8
Kacper Kafara <kacperkafara@gmail.com>
2023-11-10 10:01:35 +0100

    Just stash changes

--- app/src/main/java/com/foo/bar/MainActivity.kt
+++ app/src/main/java/com/foo/bar/MainActivity.kt
@@ -2,25 +2,54 @@ package com.foo.bar
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
 import com.foo.bar.databinding.ActivityMainBinding
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var openModalButton: Button
+    private lateinit var openContentButton: Button
+    private lateinit var toolbar: MaterialToolbar
+
+    private lateinit var layout: CoordinatorLayout
+    private lateinit var appbarLayout: AppBarLayout
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            Log.i(TAG, "handleOnBackPressed invoked")
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         openModalButton = mainBinding.openButton
+        toolbar = mainBinding.toolbar
+        openContentButton = mainBinding.openFragmentButton
+
+        toolbar.apply {
+            title = "AppTitle"
+            isTitleCentered = true
+            setBackgroundColor(Color.LTGRAY)
+        }
 
         openModalButton.setOnClickListener {
 //            this.createAndShowModal()
@@ -28,8 +57,44 @@ class MainActivity : AppCompatActivity() {
             createAndShowModalWithFragmentTransaction()
         }
 
+        openContentButton.setOnClickListener {
+//            val contentFragment = ContentFragment()
+//            Log.i(TAG, "onClickListener for openContentFragment")
+//            supportFragmentManager.commit {
+//                setReorderingAllowed(true)
+//                add(contentFragment, "ContentFragmentTag")
+//            }
+            onBackPressedCallback.isEnabled = !onBackPressedCallback.isEnabled
+        }
 
         setContentView(mainBinding.root)
+
+//        layout = CoordinatorLayout(this)
+//        layout.layoutParams = CoordinatorLayout.LayoutParams(
+//            CoordinatorLayout.LayoutParams.MATCH_PARENT,
+//            CoordinatorLayout.LayoutParams.MATCH_PARENT
+//        )
+//
+//        appbarLayout = AppBarLayout(this)
+//        appbarLayout.layoutParams = AppBarLayout.LayoutParams(
+//            AppBarLayout.LayoutParams.MATCH_PARENT,
+//            AppBarLayout.LayoutParams.WRAP_CONTENT
+//        )
+//
+//        toolbar = MaterialToolbar(this).apply {
+//            title = "AppTitle"
+//            isTitleCentered = true
+//        }
+//
+//        appbarLayout.addView(toolbar)
+//        layout.addView(appbarLayout)
+//
+//        setContentView(layout)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
     private fun createAndShowModalWithFragmentTransaction() {
@@ -48,10 +113,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun attachSomeContent() {
 //       mainBinding.root.addView()
-        mainBinding.root.addView(createModalViewContent(), ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        ))
+        mainBinding.root.addView(
+            createModalViewContent(), ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        )
         mainBinding.root.requestLayout()
     }
 
@@ -64,12 +131,20 @@ class MainActivity : AppCompatActivity() {
         frameLayout.setBackgroundColor(Color.BLUE)
 //        coordinatorLayout.setBackgroundColor(Color.YELLOW)
 
-        frameLayout.addView(view, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT))
+        frameLayout.addView(
+            view,
+            FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+        )
 
-        coordinatorLayout.addView(frameLayout, CoordinatorLayout.LayoutParams(
-            CoordinatorLayout.LayoutParams.MATCH_PARENT,
-            CoordinatorLayout.LayoutParams.MATCH_PARENT
-        ))
+        coordinatorLayout.addView(
+            frameLayout, CoordinatorLayout.LayoutParams(
+                CoordinatorLayout.LayoutParams.MATCH_PARENT,
+                CoordinatorLayout.LayoutParams.MATCH_PARENT
+            )
+        )
 
         val frameLayoutParams = frameLayout.layoutParams as CoordinatorLayout.LayoutParams
         frameLayoutParams.behavior = BottomSheetBehavior<FrameLayout>()
@@ -86,7 +161,10 @@ class MainActivity : AppCompatActivity() {
     private fun createContentView(): View {
         val view = View(this)
         view.setBackgroundColor(Color.DKGRAY)
-        view.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        view.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
         view.setBackgroundColor(Color.DKGRAY)
         return view
     }
