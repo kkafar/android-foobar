430537805a6485945ac89dec548ca9a8282468d0
Kacper Kafara <kacperkafara@gmail.com>
2023-11-18 15:25:09 +0100

    Display clean screen

--- app/src/main/java/com/foo/bar/MainActivity.kt
+++ app/src/main/java/com/foo/bar/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.foo.bar
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -18,141 +21,150 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-
-    private lateinit var mainBinding: ActivityMainBinding
-    private lateinit var openModalButton: Button
-    private lateinit var openContentButton: Button
-    private lateinit var toolbar: MaterialToolbar
-
-    private lateinit var layout: CoordinatorLayout
-    private lateinit var appbarLayout: AppBarLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        openModalButton = mainBinding.openButton
-        toolbar = mainBinding.toolbar
-        openContentButton = mainBinding.openFragmentButton
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 
-        toolbar.apply {
-            title = "AppTitle"
-            isTitleCentered = true
-            setBackgroundColor(Color.LTGRAY)
-        }
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-        openModalButton.setOnClickListener {
-//            this.createAndShowModal()
-//            attachSomeContent()
-            createAndShowModalWithFragmentTransaction()
-        }
-
-        openContentButton.setOnClickListener {
-//            val contentFragment = ContentFragment()
-//            Log.i(TAG, "onClickListener for openContentFragment")
-//            supportFragmentManager.commit {
-//                setReorderingAllowed(true)
-//                add(contentFragment, "ContentFragmentTag")
-//            }
-        }
-
-        setContentView(mainBinding.root)
-
-//        layout = CoordinatorLayout(this)
-//        layout.layoutParams = CoordinatorLayout.LayoutParams(
-//            CoordinatorLayout.LayoutParams.MATCH_PARENT,
-//            CoordinatorLayout.LayoutParams.MATCH_PARENT
-//        )
+//    private lateinit var mainBinding: ActivityMainBinding
+//    private lateinit var openModalButton: Button
+//    private lateinit var openContentButton: Button
+//    private lateinit var toolbar: MaterialToolbar
 //
-//        appbarLayout = AppBarLayout(this)
-//        appbarLayout.layoutParams = AppBarLayout.LayoutParams(
-//            AppBarLayout.LayoutParams.MATCH_PARENT,
-//            AppBarLayout.LayoutParams.WRAP_CONTENT
-//        )
+//    private lateinit var layout: CoordinatorLayout
+//    private lateinit var appbarLayout: AppBarLayout
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+//        openModalButton = mainBinding.openButton
+//        toolbar = mainBinding.toolbar
+//        openContentButton = mainBinding.openFragmentButton
 //
-//        toolbar = MaterialToolbar(this).apply {
+//        toolbar.apply {
 //            title = "AppTitle"
 //            isTitleCentered = true
+//            setBackgroundColor(Color.LTGRAY)
 //        }
 //
-//        appbarLayout.addView(toolbar)
-//        layout.addView(appbarLayout)
+//        openModalButton.setOnClickListener {
+////            this.createAndShowModal()
+////            attachSomeContent()
+//            createAndShowModalWithFragmentTransaction()
+//        }
 //
-//        setContentView(layout)
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    private fun createAndShowModalWithFragmentTransaction() {
-        val modalBottomSheet = ModalBottomSheet(createContentView())
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            add(modalBottomSheet, ModalBottomSheet.TAG)
-        }
-    }
-
-    private fun createAndShowModal() {
-        val modalBottomSheet = ModalBottomSheet()
-        modalBottomSheet.show(supportFragmentManager, ModalBottomSheet.TAG)
-    }
-
-    private fun attachSomeContent() {
-//       mainBinding.root.addView()
-        mainBinding.root.addView(
-            createModalViewContent(), ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-        )
-        mainBinding.root.requestLayout()
-    }
-
-    private fun createModalViewContent(): View {
-        val coordinatorLayout = CoordinatorLayout(this)
-        val frameLayout = FrameLayout(this)
-        val view = View(this)
-
-        view.setBackgroundColor(Color.GREEN)
-        frameLayout.setBackgroundColor(Color.BLUE)
-//        coordinatorLayout.setBackgroundColor(Color.YELLOW)
-
-        frameLayout.addView(
-            view,
-            FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT
-            )
-        )
-
-        coordinatorLayout.addView(
-            frameLayout, CoordinatorLayout.LayoutParams(
-                CoordinatorLayout.LayoutParams.MATCH_PARENT,
-                CoordinatorLayout.LayoutParams.MATCH_PARENT
-            )
-        )
-
-        val frameLayoutParams = frameLayout.layoutParams as CoordinatorLayout.LayoutParams
-        frameLayoutParams.behavior = BottomSheetBehavior<FrameLayout>()
-
-        val behavior = BottomSheetBehavior.from(frameLayout)
-        behavior.state = BottomSheetBehavior.STATE_EXPANDED
-
-        coordinatorLayout.requestLayout()
-
-
-        return coordinatorLayout
-    }
-
-    private fun createContentView(): View {
-        val view = View(this)
-        view.setBackgroundColor(Color.DKGRAY)
-        view.layoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        )
-        view.setBackgroundColor(Color.DKGRAY)
-        return view
-    }
+//        openContentButton.setOnClickListener {
+////            val contentFragment = ContentFragment()
+////            Log.i(TAG, "onClickListener for openContentFragment")
+////            supportFragmentManager.commit {
+////                setReorderingAllowed(true)
+////                add(contentFragment, "ContentFragmentTag")
+////            }
+//        }
+//
+//        setContentView(mainBinding.root)
+//
+////        layout = CoordinatorLayout(this)
+////        layout.layoutParams = CoordinatorLayout.LayoutParams(
+////            CoordinatorLayout.LayoutParams.MATCH_PARENT,
+////            CoordinatorLayout.LayoutParams.MATCH_PARENT
+////        )
+////
+////        appbarLayout = AppBarLayout(this)
+////        appbarLayout.layoutParams = AppBarLayout.LayoutParams(
+////            AppBarLayout.LayoutParams.MATCH_PARENT,
+////            AppBarLayout.LayoutParams.WRAP_CONTENT
+////        )
+////
+////        toolbar = MaterialToolbar(this).apply {
+////            title = "AppTitle"
+////            isTitleCentered = true
+////        }
+////
+////        appbarLayout.addView(toolbar)
+////        layout.addView(appbarLayout)
+////
+////        setContentView(layout)
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//    }
+//
+//    private fun createAndShowModalWithFragmentTransaction() {
+//        val modalBottomSheet = ModalBottomSheet(createContentView())
+//        supportFragmentManager.commit {
+//            setReorderingAllowed(true)
+//            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+//            add(modalBottomSheet, ModalBottomSheet.TAG)
+//        }
+//    }
+//
+//    private fun createAndShowModal() {
+//        val modalBottomSheet = ModalBottomSheet()
+//        modalBottomSheet.show(supportFragmentManager, ModalBottomSheet.TAG)
+//    }
+//
+//    private fun attachSomeContent() {
+////       mainBinding.root.addView()
+//        mainBinding.root.addView(
+//            createModalViewContent(), ViewGroup.LayoutParams(
+//                ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT
+//            )
+//        )
+//        mainBinding.root.requestLayout()
+//    }
+//
+//    private fun createModalViewContent(): View {
+//        val coordinatorLayout = CoordinatorLayout(this)
+//        val frameLayout = FrameLayout(this)
+//        val view = View(this)
+//
+//        view.setBackgroundColor(Color.GREEN)
+//        frameLayout.setBackgroundColor(Color.BLUE)
+////        coordinatorLayout.setBackgroundColor(Color.YELLOW)
+//
+//        frameLayout.addView(
+//            view,
+//            FrameLayout.LayoutParams(
+//                FrameLayout.LayoutParams.MATCH_PARENT,
+//                FrameLayout.LayoutParams.WRAP_CONTENT
+//            )
+//        )
+//
+//        coordinatorLayout.addView(
+//            frameLayout, CoordinatorLayout.LayoutParams(
+//                CoordinatorLayout.LayoutParams.MATCH_PARENT,
+//                CoordinatorLayout.LayoutParams.MATCH_PARENT
+//            )
+//        )
+//
+//        val frameLayoutParams = frameLayout.layoutParams as CoordinatorLayout.LayoutParams
+//        frameLayoutParams.behavior = BottomSheetBehavior<FrameLayout>()
+//
+//        val behavior = BottomSheetBehavior.from(frameLayout)
+//        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+//
+//        coordinatorLayout.requestLayout()
+//
+//
+//        return coordinatorLayout
+//    }
+//
+//    private fun createContentView(): View {
+//        val view = View(this)
+//        view.setBackgroundColor(Color.DKGRAY)
+//        view.layoutParams = ViewGroup.LayoutParams(
+//            ViewGroup.LayoutParams.MATCH_PARENT,
+//            ViewGroup.LayoutParams.MATCH_PARENT
+//        )
+//        view.setBackgroundColor(Color.DKGRAY)
+//        return view
+//    }
 }
