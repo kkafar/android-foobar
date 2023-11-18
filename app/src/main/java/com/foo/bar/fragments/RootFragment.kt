a5dc57f8c4cdc4a734c9c52ea93d141339138a1d
Kacper Kafara <kacperkafara@gmail.com>
2023-11-18 18:43:20 +0100

    Take buttons out ouf the AppBarLayout (#4)

--- app/src/main/java/com/foo/bar/fragments/RootFragment.kt
+++ app/src/main/java/com/foo/bar/fragments/RootFragment.kt
@@ -2,10 +2,12 @@ package com.foo.bar.fragments
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
@@ -18,6 +20,7 @@ import com.google.android.material.transition.MaterialSharedAxis
 class RootFragment(private val light: Boolean) : Fragment() {
     private lateinit var containerView: CoordinatorLayout
     private lateinit var appBarLayout: AppBarLayout
+    private lateinit var linearLayout: LinearLayout
 
     constructor() : this(true)
 
@@ -25,6 +28,8 @@ class RootFragment(private val light: Boolean) : Fragment() {
         super.onCreate(savedInstanceState)
         enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
         exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+        reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
     }
 
     override fun onCreateView(
@@ -35,8 +40,6 @@ class RootFragment(private val light: Boolean) : Fragment() {
         // Linear layout with few buttons allowing for basic navigation
         initializeContainerView()
         setBackgroundColor()
-
-        createNavigationButtons().forEach { appBarLayout.addView(it) }
 
         return containerView
     }
@@ -56,6 +59,21 @@ class RootFragment(private val light: Boolean) : Fragment() {
             )
         }
         containerView.addView(appBarLayout)
+
+        linearLayout = LinearLayout(requireContext()).apply {
+            layoutParams = CoordinatorLayout.LayoutParams(
+                CoordinatorLayout.LayoutParams.MATCH_PARENT,
+                CoordinatorLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+//                anchorId = appBarLayout.id
+//                anchorGravity = Gravity.BOTTOM
+                gravity = Gravity.CENTER_VERTICAL
+            }
+            orientation = LinearLayout.VERTICAL
+        }
+        createNavigationButtons().forEach { linearLayout.addView(it) }
+
+        containerView.addView(linearLayout)
     }
 
     private fun setBackgroundColor() {
@@ -87,10 +105,11 @@ class RootFragment(private val light: Boolean) : Fragment() {
     }
 
     private fun createButton(text: String, onClickListener: View.OnClickListener): Button {
+        Log.i(TAG, "createButton")
         val button = Button(context).apply {
             layoutParams = AppBarLayout.LayoutParams(
-                AppBarLayout.LayoutParams.MATCH_PARENT,
-                AppBarLayout.LayoutParams.WRAP_CONTENT
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
             )
             this.text = text
             setOnClickListener(onClickListener)
