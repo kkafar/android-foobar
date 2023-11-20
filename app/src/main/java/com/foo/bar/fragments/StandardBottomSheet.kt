f27b7855ce698600fca70ab6bef22698d7e902cc
Kacper Kafara <kacperkafara@gmail.com>
2023-11-20 10:14:26 +0100

    Add content view for stnadard bottom sheet

--- app/src/main/java/com/foo/bar/fragments/StandardBottomSheet.kt
+++ app/src/main/java/com/foo/bar/fragments/StandardBottomSheet.kt
@@ -6,15 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.foo.bar.R
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class StandardBottomSheet : Fragment() {
     private lateinit var containerView: CoordinatorLayout
+    private lateinit var bottomSheet: ViewGroup
     private lateinit var contentView: ViewGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i(TAG, "onCreate")
         super.onCreate(savedInstanceState)
@@ -32,8 +36,12 @@ class StandardBottomSheet : Fragment() {
 
     private fun initializeViewHierarchy() {
         initializeContainerView()
+        initializeBottomSheet()
         initializeContentView()
-        containerView.addView(contentView)
+
+        bottomSheet.addView(contentView)
+        containerView.addView(bottomSheet)
+
     }
 
     private fun initializeContainerView() {
@@ -45,8 +53,8 @@ class StandardBottomSheet : Fragment() {
         }
     }
 
-    private fun initializeContentView() {
-        contentView = FrameLayout(requireContext()).apply {
+    private fun initializeBottomSheet() {
+        bottomSheet = FrameLayout(requireContext()).apply {
             layoutParams = CoordinatorLayout.LayoutParams(
                 CoordinatorLayout.LayoutParams.MATCH_PARENT,
                 CoordinatorLayout.LayoutParams.MATCH_PARENT,
@@ -59,7 +67,44 @@ class StandardBottomSheet : Fragment() {
                 }
             }
         }
+//        bottomSheet.setBackgroundColor(requireContext().resources.getColor(R.color.colorSecondary, requireContext().theme))
+    }
+
+    private fun initializeContentView() {
+        contentView = LinearLayout(requireContext()).apply {
+            layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+            )
+            orientation = LinearLayout.VERTICAL
+        }
+
+        createButtonsForContentView().forEach {
+            contentView.addView(it)
+        }
         contentView.setBackgroundColor(requireContext().resources.getColor(R.color.colorSecondary, requireContext().theme))
+    }
+
+    private fun createButtonsForContentView(): List<Button> {
+        val res = requireContext().resources
+        return listOf(
+           createButton(res.getString(R.string.pop_fragment)) {
+               Log.i(TAG, "Popping StandardBottomSheet")
+           }
+        )
+    }
+
+    private fun createButton(text: String, onClickListener: View.OnClickListener): Button {
+        Log.i(RootFragment.TAG, "createButton")
+        val button = Button(context).apply {
+            layoutParams = AppBarLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            this.text = text
+            setOnClickListener(onClickListener)
+        }
+        return button
     }
 
     companion object {
